f701ec628c22d5cf76f9693cec846e5eac198ff4
DX <danielxu.314@gmail.com>
2022-05-04 08:24:38 -0700

    added a line

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -51,8 +51,10 @@ public class MarkdownParse {
         return toReturn;
     }
     
+    
 
     public static void main(String[] args) throws IOException {
+        System.out.println("github test");
         Path fileName = Path.of(args[0]);
         String content = Files.readString(fileName);
         ArrayList<String> links = getLinks(content);
