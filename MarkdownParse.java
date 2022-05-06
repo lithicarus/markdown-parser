2cd6a4fe6b701f4af5869052f46eed8aa405e854
DX <danielxu.314@gmail.com>
2022-05-06 10:07:34 -0700

    added junit test lines

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -1,4 +1,8 @@
 //https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
+/**
+ * javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar MarkdownParseTest.java
+java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore MarkdownParseTest
+ */
 
 import java.io.IOException;
 import java.nio.file.Files;
