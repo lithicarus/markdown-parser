e7fe726ea039fd15855269a002a1e0c623385905
Kameron B Gano <cs15lsp22acn@ieng6-201.ucsd.edu>
2022-05-06 10:28:58 -0700

    Adding makefile

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -2,6 +2,7 @@
 /**
  * javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar MarkdownParseTest.java
 java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore MarkdownParseTest
+ * Hello world
  */
 
 import java.io.IOException;
